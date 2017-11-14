25cd7f64993cafe33165a1c80fba18e31c0624ff
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-11-14 23:13:35 +0800

    add write interface

--- index.js
+++ index.js
@@ -2,27 +2,96 @@
  * read lines by file path
  */
 const debug = require('debug')('readlineq');
+const fs = require('fs');
 
- exports = module.exports = function(file_path) {
+/**
+ * read file into lines
+ * @param  {[type]} file_path [description]
+ * @return {[type]}           [description]
+ */
+function _read(file_path) {
     var lines = []
     return new Promise((resolve, reject) => {
         var lineReader = require('readline').createInterface({
             input: require('fs').createReadStream(file_path)
         });
 
-        lineReader.on('line', function (line) {
+        lineReader.on('line', function(line) {
             debug('Line from file: %s', line);
             lines.push(line.trim());
         });
 
-        lineReader.on('close', function (err) {
+        lineReader.on('close', function(err) {
             debug('all lines are read, line size %d', lines.length);
             resolve(lines);
         })
 
-        lineReader.on('error', function (err) {
+        lineReader.on('error', function(err) {
             debug('error happens', err);
             reject(err)
         })
     });
+}
+
+// const JSONStream = require('JSONStream');
+/**
+ * Dump huge array to file
+ * @param {*} to dump obj to file path
+ * @param {*} obj JSONObject or JSONArray
+ */
+// function _write(to, obj) {
+//     return new Promise((resolve, reject) => {
+//         var records = obj
+//         var transformStream = JSONStream.stringify();
+//         var outputStream = fs.createWriteStream(to);
+//         transformStream.pipe(outputStream);
+//         records.forEach(transformStream.write);
+//         transformStream.end();
+
+//         outputStream.on(
+//             "finish",
+//             function handleFinish() {
+//                 resolve();
+//             }
+//         );
+
+//         outputStream.on(
+//             "error",
+//             function handleFinish(err) {
+//                 reject(err);
+//             }
+//         );
+//     });
+// }
+// 
+/**
+ * Write lines to file
+ * @param  {[type]} from_ [description]
+ * @param  {[type]} to_   [description]
+ * @return {[type]}       [description]
+ */
+function _write(lines, to_) {
+    return new Promise((resolve, reject) => {
+        var t = fs.createWriteStream(to_);
+        t.on('open', function(fd) {
+            for (let x of lines) {
+                t.write(x);
+            }
+            t.end();
+            resolve()
+        });
+    })
+}
+
+/**
+ * write or read data
+ * @param  {[type]} file_path [description]
+ * @param  {[type]} obj       [description]
+ * @return {[type]}           [description]
+ */
+exports = module.exports = function(file_path, obj) {
+    if (obj) {
+        return _write(obj, file_path)
+    }
+    return _read(file_path);
 }
--- test/index.js
+++ test/index.js
@@ -6,3 +6,10 @@ test('Readlines # read', async (t)=>{
     // console.log(lines);
     t.pass()
 })
+
+test('Readlines # write', async (t)=>{
+    var readlineq = require('../index.js');
+    var lines = await readlineq('./tmp/test.txt', ["foo\n", "bar\n"]);
+    // console.log(lines);
+    t.pass()
+})
